ae1b6548b948acea75c8f8dd31757072def430ae
claymence <clay.dev@mailbox.org>
2024-06-24 15:04:21 +0200

    implemented Chirps

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # clay-laravel
 a web app made with laravel
+
+## overview
+
+## to-do
+change class to "implements ShouldQueue" @ app\Listeners\SendChirpCreatedNotifications.php
+-> requires to run "php artisan queue:work" afterwards
+
+pagination for chirps list?
+
---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SendChirpCreatedNotifications
+//class SendChirpCreatedNotifications implements ShouldQueue
+// should use "implements ShouldQueue" - not using it while testing so "php artisan queue:work" isnt needed
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void 
+    {
+        /* foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        } */
+
+        Log::info('New Chirp created. Notification for users is commented out.');
+        // need to check out logging features and setup in config\logging.php
+    }
+}
