749845e21cfd74d950600bcc26fad91b9239ee80
claymence <clay.dev@mailbox.org>
2024-07-03 01:02:47 +0200

    X-Content-Type-Options, Referrer-Policy

--- app/Http/Middleware/SecurityHeadersMiddleware.php
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -20,22 +20,29 @@ class SecurityHeadersMiddleware
     {
         $response = $next($request);
 
+        $response->headers->set('X-Frame-Options', 'DENY');
+
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+
+        $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
+
         //nonce globally shared from app\Providers\AppServiceProvider.php
         $nonce = View::shared('nonce');
 
-        //$response->headers->set('X-Frame-Options', 'DENY');
-
-        //$response->headers->set('Content-Security-Policy', "default-src 'self' 'nonce-$nonce'; script-src 'self' 'nonce-$nonce'; style-src 'self' 'nonce-$nonce'; font-src 'self' 'nonce-$nonce' https://fonts.bunny.net/; object-src 'none'; frame-ancestors 'none'");
+        /* $response->headers->set(
+            'Content-Security-Policy', 
+            "default-src 'self' 'nonce-$nonce'; script-src 'self' 'nonce-$nonce'; style-src 'self' 'nonce-$nonce'; font-src 'self' 'nonce-$nonce' https://fonts.bunny.net/; object-src 'none'; frame-ancestors 'none'"
+        ); */
         
         /* $response->headers->set(
             'Content-Security-Policy', 
             "font-src 'self' 'nonce-$nonce' https://fonts.bunny.net/; object-src 'none'; frame-ancestors 'none'"
-            ); */
+        ); */
 
         $response->headers->set(
             'Content-Security-Policy', 
-            "font-src 'self' https://fonts.bunny.net/; object-src 'none'; frame-ancestors 'none'"
-            );
+            "font-src 'self' 'nonce-$nonce' https://fonts.bunny.net/; object-src 'none'; frame-ancestors 'none'"
+        );
 
         return $response;
     }
