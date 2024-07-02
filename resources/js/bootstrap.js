8d0559a097a076124ac45f88fd172bcac1e55eed
claymence <clay.dev@mailbox.org>
2024-07-02 21:23:39 +0200

    added x-frame-options and content-security-policy to axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// added after security check
+window.axios.defaults.headers.common['X-Frame-Options'] = 'DENY';
+window.axios.defaults.headers.common['Content-Security-Policy'] = "frame-ancestors 'none'";
