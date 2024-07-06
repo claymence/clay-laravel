5d4d259d72077402858e02021d61e8331bf3fe4b
claymence <clay.dev@mailbox.org>
2024-07-06 19:55:00 +0200

    build debug

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/assets/images/**/*.*',
+                'resources/assets/*',
             ],
             refresh: true,
         }),
