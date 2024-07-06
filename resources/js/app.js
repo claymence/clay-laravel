d0f1daeeffa4412d2c88464ae5e6b3fe02f0a960
claymence <clay.dev@mailbox.org>
2024-07-06 20:30:14 +0200

    build debug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,12 @@
 import './bootstrap';
 import.meta.glob([
-    '../assets/**',
-  ]);
+    '../images/**',
+]);
+/* 
+
+https://laravel.com/docs/11.x/vite#blade-processing-static-assets
+
+These assets will now be processed by Vite when running npm run build. 
+You can then reference these assets in Blade templates using the Vite::asset method.
+
+ */
