dc9e41a8477903613a2c00d6c02c0c632c1dda42
claymence <clay.dev@mailbox.org>
2024-07-11 23:29:18 +0200

    comments

--- app/Livewire/Jwst/JwstApiTest.php
+++ app/Livewire/Jwst/JwstApiTest.php
@@ -49,61 +49,26 @@ class JwstApiTest extends Component
     }
 }
 
-/* class JwstApiTest extends Component
+
+// TEST CODE - test with api call below successful
+
+/* public function fetchData()
 {
-    public $data = [];
-    public $error = null;
+    $response = Http::withHeaders([
+        'X-API-KEY' => env('JWST_API_KEY')
+    ])->get('https://api.jwstapi.com/suffix/list');
 
-    public function fetchData($page = 1, $perPage = 15)
-    {
-        $response = Http::withHeaders([
-            'X-API-KEY' => env('JWST_API_KEY')
-        ])->get('https://api.jwstapi.com/all/suffix/_thumb', [
-            'page' => $page,
-            'perPage' => $perPage
-        ]);
-
-        if ($response->successful()) {
-            $thumbnails = collect($response->json()['body'])->filter(function ($item) {
-                return $item['suffix'] === '_thumb' && $item['file_type'] === 'jpg';
-            });
-
-            $this->data = $thumbnails->all();
-        } else {
-            $this->error = $response->body();
-        }
-    }
-
-    public function render()
-    {
-        return view('livewire.jwst.jwst-api-test', [
-            'data' => collect($this->data)->paginate(15),
-            'error' => $this->error
-        ]);
+    if ($response->successful()) {
+        $this->data = $response->json()['body'];
+    } else {
+        $this->error = $response->body();
     }
 } */
 
-
-
-    // TEST CODE - test with api call below successful
-
-    /* public function fetchData()
-    {
-        $response = Http::withHeaders([
-            'X-API-KEY' => env('JWST_API_KEY')
-        ])->get('https://api.jwstapi.com/suffix/list');
-
-        if ($response->successful()) {
-            $this->data = $response->json()['body'];
-        } else {
-            $this->error = $response->body();
-        }
-    } */
-
-    /* public function render()
-    {
-        return view('livewire.jwst.jwst-api-test', [
-            'data' => $this->data,
-            'error' => $this->error
-        ]);
-    } */
+/* public function render()
+{
+    return view('livewire.jwst.jwst-api-test', [
+        'data' => $this->data,
+        'error' => $this->error
+    ]);
+} */
